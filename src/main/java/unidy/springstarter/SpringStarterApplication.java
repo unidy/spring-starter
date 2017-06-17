dc191c6f22c6bf67175115b5ff79fec075440261
RaydonZhu <raydon@hubub.com>
2017-06-17 17:52:21 -0400

    spring starter project init

---
+++ src/main/java/unidy/springstarter/SpringStarterApplication.java
@@ -0,0 +1,13 @@
+package unidy.springstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarterApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+		SpringApplication.run(SpringStarterApplication.class, args);
+	}
+}
